a83a07c959075ea48b9e1bc1e1ba865185c9d2cc
alexqs96 <alex.rps96@gmail.com>
2023-05-02 11:44:56 -0300

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -22,12 +22,12 @@ export default function Home() {
         isLoading?
         <p>Cargando..</p>
         :
-        <div className='flex flex-wrap gap-2'>
+        <div className='flex flex-wrap gap-2 max-md:justify-between'>
           {
             strapi?.data?
             Children.toArray(
-              strapi.data.map(e => (
-                <Link href="/" className='shadow-lg hover:shadow-xl border border-transparent dark:border-white/20 hover:dark:border-white/50 rounded transition-all duration-300 overflow-hidden w-48'>
+              strapi.data.map((e,index) => (
+                <Link href="/" className={(strapi.data.length-1 === index && strapi.data.length % 2 ? "max-md:mr-auto" : "")+' shadow-lg hover:shadow-xl border border-transparent dark:border-white/20 hover:dark:border-white/50 rounded transition-all duration-300 overflow-hidden max-sm:w-[42vw] w-48'}>
                   <Image
                     width={64}
                     height={64}
@@ -37,7 +37,7 @@ export default function Home() {
                     src={e.attributes.foto? e.attributes.foto : "/img/apu_profile.png"}              
                     alt={"Foto de "+e.attributes.nombre}
                   />
-                  <div className='p-4 flex flex-col gap-y-1.5'>
+                  <div className='p-4 max-md:p-2 flex flex-col gap-y-1.5 max-md:text-sm'>
                     <p className='capitalize font-medium'>{e.attributes.nombre}</p>
                     <p className='font-semibold'> $300 ARS <small className='line-through'>$200</small></p>
                     <button className='p-2 flex justify-center gap-2 items-center flex-wrap bg-indigo-600 text-white rounded'>
